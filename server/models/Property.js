c9cae64a2495c5cb4d15867f3fa97039a1b071a2
Aman <amsingh714@gmail.com>
2023-09-06 19:01:09 -0400

    added property model

---
+++ server/models/Property.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const PropertySchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "User ID is required"],
+  },
+  name: {
+    type: String,
+    required: [true, "Please provide the name of the gas station"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please provide the address of the gas station"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
