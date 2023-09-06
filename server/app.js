7d896264ee0802998527911a7838906450c5743d
Aman <amsingh714@gmail.com>
2023-09-06 15:01:00 -0400

    added basic auth routes and controller

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const morgan = require("morgan");
 
 const connectDB = require("./db/connect");
+const authRouter = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 const port = process.env.PORT || 3000;
 
---
+++ server/controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require("../models/User");
+
+const register = async (req, res) => {
+  
+};
+
+const login = async (req, res) => {};
+
+const logout = async (req, res) => {};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
