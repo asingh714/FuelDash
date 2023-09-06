2ba7a8c0726e0fa221fda6dee1c4e7153e53148b
Aman <amsingh714@gmail.com>
2023-09-06 12:53:06 -0400

    connected to db

--- server/app.js
+++ server/app.js
@@ -1,1 +1,17 @@
-console.log("test");
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+
+const connectDB = require("./db/connect");
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is running on port ${port}`);
+});
