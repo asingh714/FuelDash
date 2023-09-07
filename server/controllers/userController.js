a94aafd177dc32d61567affacb1cb40a55db4eb9
Aman <amsingh714@gmail.com>
2023-09-07 15:10:02 -0400

    able to update user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,12 @@
 const User = require("../models/User");
+const validator = require("validator");
 
 const updatePassword = async (req, res) => {
   const userId = req.user.userId;
 
   const { currentPassword, newPassword } = req.body;
 
-  if (!currentPassword || !newPassword) {
+  if (!currentPassword || !newPassword || newPassword.length < 6) {
     return res
       .status(400)
       .json({ msg: "Please provide current password and new password" });
@@ -22,7 +23,7 @@ const updatePassword = async (req, res) => {
   if (!isPasswordValid) {
     return res.status(400).json({ msg: "Invalid credentials" });
   }
-  
+
   try {
     user.password = newPassword;
 
@@ -34,6 +35,40 @@ const updatePassword = async (req, res) => {
   }
 };
 
+const updateUser = async (req, res) => {
+  const userId = req.user.userId;
+
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ msg: "Please provide name" });
+  }
+
+  const isEmailValid = validator.isEmail(email);
+  if (!isEmailValid) {
+    return res.status(400).json({ msg: "Please provide a valid email" });
+  }
+
+  const user = await User.findOne({ _id: userId });
+
+  if (!user) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { name, email },
+      { new: true, runValidators: true }
+    );
+
+    res.status(200).json({ updatedUser });
+  } catch {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
 module.exports = {
   updatePassword,
+  updateUser,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { authenticate } = require("../middleware/authentication");
-const { updatePassword } = require("../controllers/userController");
+const { updatePassword, updateUser } = require("../controllers/userController");
 
 router.route("/updatePassword").patch(authenticate, updatePassword);
+router.route("/updateUser").patch(authenticate, updateUser);
 
 module.exports = router;
