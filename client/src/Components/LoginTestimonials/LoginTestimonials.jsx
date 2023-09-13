29ce54597ed7535a7b3a5d31b149aa2d68a07515
Aman <amsingh714@gmail.com>
2023-09-13 16:53:40 -0400

    styled login page

--- client/src/Components/LoginTestimonials/LoginTestimonials.jsx
+++ client/src/Components/LoginTestimonials/LoginTestimonials.jsx
@@ -7,6 +7,7 @@ const testimonials = [
     review:
       "I was always behind on tracking my sales data, but FuelDash has completely changed the game for me. The real-time analytics are incredibly helpful. Now, I make informed decisions quickly and have seen my profits go up!",
     name: "John Doe",
+    img: "https://randomuser.me/api/portraits/men/19.jpg",
     company: "Pine Valley Gas & Go",
     location: "Omaha, NE",
   },
@@ -14,6 +15,7 @@ const testimonials = [
     review:
       "I can't believe how easy it is to use FuelDash. The dashboard gives me everything I need to know, from my best-selling items to payment types. It's like having an extra employee that works 24/7!",
     name: "Jane Smith",
+    img: "https://randomuser.me/api/portraits/women/23.jpg",
     company: "Lakeside Fuels",
     location: "Chicago, IL",
   },
@@ -21,6 +23,7 @@ const testimonials = [
     review:
       "Any time I've had a question or needed help, FuelDash's customer service has been top-notch. The platform itself is a lifesaver, but the support team makes it even better.",
     name: "Carlos Rodriguez",
+    img: "https://randomuser.me/api/portraits/men/61.jpg",
     company: "Metro Fuel Stop",
     location: "Miami, FL",
   },
@@ -28,6 +31,7 @@ const testimonials = [
     review:
       "Before FuelDash, I had to rely on gut feeling and rough calculations. Now, I get insights that even some of the big chains would envy. Couldn't be happier with this tool!",
     name: "Emily Brown",
+    img: "https://randomuser.me/api/portraits/women/79.jpg",
     company: "Route 66 Gas & Snacks",
     location: "Flagstaff, AZ",
   },
@@ -44,14 +48,15 @@ const LoginTestimonials = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  const { review, name, company, location } = testimonials[currentIndex];
+  const { review, name, company, location, img } = testimonials[currentIndex];
 
   return (
     <div className="login-testimonial-container">
-      <p>{review}</p>
-      <span>{name}</span>
-      <span>{company},</span>
-      <span>{location}</span>
+      <p>&ldquo;{review}&ldquo;</p>
+      <img src={img} alt="Satisfied gas station owner" />
+      <span className="name">{name}</span>
+      <span className="company">{company}</span>
+      <span className="location">{location}</span>
       <div className="circle-container">
         {testimonials.map((_, index) => (
           <div
--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -4,18 +4,20 @@ import "./Login.scss";
 
 const Login = () => {
   return (
-    <div className="login-page-container">
-      <h2 className="login-page-heading">Welcome Back</h2>
-      <div className="login-page-content-container">
-        <div className="left-container">
-          <span>
-            We are supercharging your gas station operations, one dashboard at a
-            time.
-          </span>
-          <LoginForm />
-        </div>
-        <div className="right-container">
-          <LoginTestimonials />
+    <div className="login-page">
+      <div className="login-page-container">
+        <h2 className="login-page-heading">Welcome Back</h2>
+        <div className="login-page-content-container">
+          <div className="left-container">
+            <span>
+              We are supercharging your gas station operations, one dashboard at
+              a time.
+            </span>
+            <LoginForm />
+          </div>
+          <div className="right-container">
+            <LoginTestimonials />
+          </div>
         </div>
       </div>
     </div>
