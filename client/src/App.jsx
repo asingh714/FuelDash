b51389da7fd1795bae70a3b08937783888b4ca4b
Aman <amsingh714@gmail.com>
2023-09-13 11:02:01 -0400

    added home and dashboard with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,35 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import Home from "./Pages/Home/Home.jsx";
+import Dashboard from "./Pages/Dashboard/Dashboard.jsx";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>yo</h1>
-    </>
-  );
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Outlet />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
