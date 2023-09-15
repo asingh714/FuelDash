305b923f6fee7c7f48baa966c1d591212da92bfc
Aman <amsingh714@gmail.com>
2023-09-15 14:39:23 -0400

    added hamburger menu for mobile screen

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <div className="navbar-container">
       <Link to="/" className="logo">
         <img src="/fueldash_logo.png" alt="" />
         <span>FuelDash</span>
       </Link>
+
       <div className="links-container">
         <div className="resources-container">
           <span id="resource-link">
@@ -27,6 +34,14 @@ const NavBar = () => {
         <Link to="/signup" id="signup-link">
           Start Now →
         </Link>
+
+        <div
+          className={`hamburger-menu ${menuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
       </div>
     </div>
   );
