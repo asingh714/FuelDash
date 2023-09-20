4fee1ffc91662e82831eace32adc9d8a148c741b
Aman <amsingh714@gmail.com>
2023-09-20 10:54:32 -0400

    image data updated

--- client/src/utils/imageData.js
+++ client/src/utils/imageData.js
@@ -8,7 +8,35 @@ const defaultImages = {
     "https://img.freepik.com/free-photo/close-up-image-man-s-hands-holding-tube-squeezing-whitening-toothpaste-brush_343059-2488.jpg?w=1480&t=st=1694898245~exp=1694898845~hmac=5ae27c8a41440912c731f23c218212aac4384c9583fd7ce35e7998d0d0454ede",
   "Chewing Gum":
     "https://img.freepik.com/free-photo/close-up-pretty-blond-teenager-girl-with-wavy-hair-blowing-bubble-from-gum-camera-yellow-background-candy-bubble_574295-4198.jpg?w=1800&t=st=1694898272~exp=1694898872~hmac=4a0d1e03c3ad724f6eb2e3eb80a680980d765b9d6d740a5a177b33c5c68f04f8",
-  // add more products and their image paths here
+  "Beef Jerky":
+    "https://img.freepik.com/free-photo/grilled-sirloin-steak-rustic-wood-table-fresh-delicious-generated-by-ai_188544-56192.jpg?t=st=1695220791~exp=1695224391~hmac=29130e5ed84b506343cfd507ffc5d2296e9fdd0947fe3a9aaf136ccc7148ec0a&w=1800",
+  Bread:
+    "https://img.freepik.com/free-photo/freshly-baked-bread-rustic-wooden-table-generated-by-ai_188544-41084.jpg?t=st=1695220815~exp=1695224415~hmac=ee7bc1e3c34ed4029afa6b15bd638d6349d1109dd2c2f8063dbb94668dd45b9d&w=1800",
+  Cheese:
+    "https://img.freepik.com/free-photo/close-up-delicious-white-chocolate-bar_23-2150660945.jpg?t=st=1695220878~exp=1695224478~hmac=94fca323aa5a97162d80b83a1bc9d930b5a5064e1efaf180bb3cd6e2adc71e4c&w=1800",
+  "Chocolate Bar":
+    "https://img.freepik.com/free-photo/close-up-delicious-chocolate-bar-pieces_23-2150660941.jpg?t=st=1695218011~exp=1695221611~hmac=4856a6bee2000220d0126748cb6e6aa53551f7ba8ab72f855cfbed97331c7b9e&w=996",
+  "Coca-Cola":
+    "https://img.freepik.com/free-photo/whiskey-highball-ice-citrus-freshness-depicted-generated-by-ai_188544-15170.jpg?t=st=1695220960~exp=1695224560~hmac=cb2f93287b07a78c27a8fc5abd5a8536864f4976beb8d706ce9f284f08773efc&w=1800",
+  "Engine Oil":
+    "https://img.freepik.com/free-photo/free-photo-beauty-product-bottle-mockup-image-with-background_1340-31233.jpg?t=st=1695221008~exp=1695224608~hmac=ee4ca0791b734f5b62230c4ac615520400b948a46e5e56ac6bfb1454a0193959&w=1800",
+  Magazine:
+    "https://img.freepik.com/free-photo/messy-stack-paperwork-old-new-generated-by-ai_188544-30270.jpg?t=st=1695221094~exp=1695224694~hmac=973b6690b01fbbfa1846048bfa5039e2b0131e609f0447fb6ccd372d5cb8989e&w=1800",
+  Milk: "https://img.freepik.com/premium-photo/premium-packaging-professional-set-elevate-your-product-designs-with-realistic-precision-bag-box_655090-105761.jpg?w=1480",
+  "Motorcycle Oil":
+    "https://img.freepik.com/free-photo/bottle-liquid-with-black-cap-silver-handle_1340-34390.jpg?t=st=1695221171~exp=1695224771~hmac=96b1b8ecd6c6759b5bdbe5011cd7ca61ddc28e547cc84728e15e3474ccec1456&w=1480",
+  "Potato Chips":
+    "https://img.freepik.com/free-photo/prepared-unhealthy-pile-delicious-food_1339-25.jpg?t=st=1695221225~exp=1695221825~hmac=3b48cb15eaaf0ed7118a20c78780052da79578afba5e24c406bdcb367013103d",
+  Shampoo:
+    "https://images.freeimages.com/variants/j1vT3BUSNRSoWJ1ezrAzUSL3/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d",
+  Sunglasses:
+    "https://images.freeimages.com/images/large-previews/170/sunglasses-1187301.jpg",
+  "Travel Pillow":
+    "https://images.freeimages.com/variants/wLSLjxDSkSw6oNoy44M21QBV/f4a36f6589a0e50e702740b15352bc00e4bfaf6f58bd4db850e167794d05993d",
+  "Water Bottle":
+    "https://images.freeimages.com/images/large-previews/62f/water-bottle-1324935.jpg",
+  "Windshield Wiper Fluid":
+    "https://images.freeimages.com/images/large-previews/306/street-scene-through-rain-glass-1192570.jpg",
 };
 
 export default defaultImages;
