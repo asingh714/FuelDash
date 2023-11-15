68d80a82fc86542805de5d783fa9cb4565d6f44b
Aman <amsingh714@gmail.com>
2023-11-15 17:04:57 -0500

    not found page styled

--- client/src/Pages/NotFound/NotFound.jsx
+++ client/src/Pages/NotFound/NotFound.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 import "./NotFound.scss";
 
 function NotFound() {
   return (
     <div className="not-found-container">
-      <h2>404: Page Not Found</h2>
+      <span>404</span>
+      <h2>Page not found</h2>
+      <p>Sorry, we couldn’t find the page you’re looking for.</p>
+      <Link to="/" className="home-link">
+        ← Back to home
+      </Link>
     </div>
   );
 }
