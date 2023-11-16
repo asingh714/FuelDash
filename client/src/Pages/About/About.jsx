ce8566bde3888e7d44f8fea1ae2d7b111af30023
Aman <amsingh714@gmail.com>
2023-11-16 13:33:06 -0500

    added values-section styles to about page

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -24,6 +24,100 @@ const About = () => {
           <img src="working.png" alt="Employee Working" />
         </div>
       </section>
+
+      <section id="mission-section">
+        <div className="mission-content">
+          <h2>Our Mission</h2>
+          <p>
+            To provide accessible, user-friendly, and powerful tools that enable
+            small and independent gas station operators to make informed
+            business decisions, enhance their operational workflows, and embrace
+            the power of analytics and real-time data.
+          </p>
+        </div>
+        <div className="mission-stats">
+          <p>
+            <strong>5,000+</strong> Stations Empowered
+          </p>
+          <p>
+            <strong>10 Million+</strong> Transactions Processed
+          </p>
+          <p>
+            <strong>99.8%</strong> Customer Satisfaction
+          </p>
+        </div>
+      </section>
+
+      <section id="values-section">
+        <img src="our-values.png" alt="Our Core Values" />
+        <h2>Our Core Values</h2>
+        <p>
+          Our commitment to excellence is driven by the values we uphold. We
+          strive for innovation, customer success, integrity, and sustainability
+          in everything we do.
+        </p>
+        <div className="values-grid">
+          <div>
+            <h3>Innovation</h3>
+            <p>
+              We believe in continuous improvement and always seek to advance
+              our platform with the latest technology.
+            </p>
+          </div>
+          <div>
+            <h3>Customer Success</h3>
+            <p>
+              Your success is our success. We&apos;re committed to providing
+              outstanding support and services to our clients.
+            </p>
+          </div>
+          <div>
+            <h3>Integrity</h3>
+            <p>
+              Honesty and transparency are at the heart of our business. We act
+              ethically and responsibly in all our interactions.
+            </p>
+          </div>
+          <div>
+            <h3>Sustainability</h3>
+            <p>
+              We aim to contribute positively to the environment and society
+              through eco-friendly business practices and community engagement.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section id="blog-section">
+        <h2>Insights and Updates</h2>
+        <div className="blog-posts">
+          <article>
+            <img src="blog-post-1.jpg" alt="Blog Post Title" />
+            <h3>Maximizing Profits with Smart Inventory Management</h3>
+            <p>
+              Discover how integrating smart inventory systems can boost your
+              gas station&apos;s profitability.
+            </p>
+          </article>
+          <article>
+            <img src="blog-post-2.jpg" alt="Blog Post Title" />
+            <h3>Adapting to the Evolving Fuel Retail Landscape</h3>
+            <p>
+              Learn about the latest trends in the fuel retail industry and how
+              to stay ahead of the curve.
+            </p>
+          </article>
+          <article>
+            <img src="blog-post-3.jpg" alt="Blog Post Title" />
+            <h3>The Future of Fuel Stations: Electric and Beyond</h3>
+            <p>
+              Explore the future of fuel stations in a world moving towards
+              electric vehicles.
+            </p>
+          </article>
+        </div>
+      </section>
+
       <Footer />
     </div>
   );
