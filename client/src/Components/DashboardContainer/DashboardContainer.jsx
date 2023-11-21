de06f970d30bef5e2bd03739e80b8145d511f979
Aman <amsingh714@gmail.com>
2023-11-21 16:47:22 -0500

    re-routing users from dashboard to login when not logged in

--- client/src/Components/DashboardContainer/DashboardContainer.jsx
+++ client/src/Components/DashboardContainer/DashboardContainer.jsx
@@ -1,4 +1,7 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../utils/AuthContext";
+import Notification from "../Notification/Notification";
 import { useQuery } from "@tanstack/react-query";
 
 import newRequest from "../../utils/newRequest";
@@ -25,6 +28,22 @@ const DashboardContainer = () => {
   const [errorMsg, setErrorMsg] = useState(null);
   const [propertyId, setPropertyId] = useState(undefined);
   const [selectedDate, setSelectedDate] = useState(getCurrentLocalDate());
+  const [notification, setNotification] = useState(null);
+
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (!currentUser) {
+      setNotification({
+        type: "error",
+        message: "You are not logged in. Redirecting to login page...",
+      });
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
+    }
+  }, [currentUser, navigate]);
 
   const { isLoading, error, data } = useQuery(
     [propertyId, selectedDate],
@@ -55,121 +74,130 @@ const DashboardContainer = () => {
   }
 
   return (
-    <div className="dashboard-whole-container">
-      <div className="dashboard-menu-container">
-        <DateSelector
-          currentDate={selectedDate}
-          onDateChange={setSelectedDate}
+    <>
+      {notification && (
+        <Notification
+          type={notification.type}
+          message={notification.message}
+          onClose={() => setNotification(null)}
         />
-
-        <PropertyDropdown
-          onPropertiesFetched={(properties) => {
-            if (properties?.length > 0 && !propertyId) {
-              setPropertyId(properties[0]._id);
-            }
-          }}
-          onPropertyChange={setPropertyId}
-          defaultSelected={propertyId}
-        />
-      </div>
-      {error ? (
-        <div className="error-message">{errorMsg}</div>
-      ) : (
-        <div className="dashboard-container">
-          <div className="box box1">
-            <TopProductSalesBox chartData={data?.topNonGasProducts} />
-          </div>
-          <div className="box box2">
-            <TinyChartBox
-              money={true}
-              color="#84cc16"
-              icon="/revenue.svg"
-              title="Revenue"
-              total={data?.totalRevenue}
-              chartData={data?.sevenDaysRevenue}
-              myDataKey="Revenue"
-              lineDataKey="date"
-              detailedPage="revenue"
-              propertyId={propertyId}
-            />
-          </div>
-          <div className="box box3">
-            <TinyChartBox
-              money={false}
-              color="#3b82f6"
-              icon="/gas-station.svg"
-              title="Gallons Sold"
-              total={data?.totalGallonsSold}
-              chartData={data?.sevenDaysTotalGallons}
-              myDataKey="Gallons Sold"
-              lineDataKey="day"
-              detailedPage="gallons"
-              propertyId={propertyId}
-            />
-          </div>
-          <div className="box box4">
-            <PieChartBox
-              title="Top Selling Gasoline Products"
-              chartData={data?.topGasProducts}
-            />
-          </div>
-          <div className="box box5">
-            <TinyChartBox
-              money={true}
-              color="#0d9488"
-              icon="/cash.svg"
-              title="Cash Payments"
-              total={data?.dailyCashPayments}
-              chartData={data?.sevenDaysPaymentTotals}
-              myDataKey="Total Cash"
-              lineDataKey="day"
-              detailedPage="cash"
-              propertyId={propertyId}
-            />
-          </div>
-          <div className="box box6">
-            <TinyChartBox
-              money={true}
-              color="#7c3aed"
-              icon="/credit-card.svg"
-              title="Credit Card Payments"
-              total={data?.dailyCreditCardPayments}
-              chartData={data?.sevenDaysPaymentTotals}
-              myDataKey="Total Credit Card"
-              lineDataKey="day"
-              detailedPage="credit"
-              propertyId={propertyId}
-            />
-          </div>
-          <div className="box box7">
-            <BarChartBox
-              chartData={data?.sevenDaysRevenue}
-              color="#0d9488"
-              title="7 Day Revenue"
-              bar="Revenue"
-              xaxis="date"
-            />
-          </div>
-          <div className="box box8">
-            <BarChartBox
-              chartData={data?.sevenDaysTotalGallons}
-              color="#3b82f6"
-              title="7 Day Gallons Sold"
-              bar="Gallons Sold"
-              xaxis="day"
-            />
-          </div>
-
-          <div className="box box9">
-            <GasInventoryBoxes propertyId={propertyId} />
-          </div>
-
-          <div className="box box10">
-            <InventoryContainer propertyId={propertyId} />
-          </div>
-        </div>
       )}
-    </div>
+      <div className="dashboard-whole-container">
+        <div className="dashboard-menu-container">
+          <DateSelector
+            currentDate={selectedDate}
+            onDateChange={setSelectedDate}
+          />
+
+          <PropertyDropdown
+            onPropertiesFetched={(properties) => {
+              if (properties?.length > 0 && !propertyId) {
+                setPropertyId(properties[0]._id);
+              }
+            }}
+            onPropertyChange={setPropertyId}
+            defaultSelected={propertyId}
+          />
+        </div>
+        {error ? (
+          <div className="error-message">{errorMsg}</div>
+        ) : (
+          <div className="dashboard-container">
+            <div className="box box1">
+              <TopProductSalesBox chartData={data?.topNonGasProducts} />
+            </div>
+            <div className="box box2">
+              <TinyChartBox
+                money={true}
+                color="#84cc16"
+                icon="/revenue.svg"
+                title="Revenue"
+                total={data?.totalRevenue}
+                chartData={data?.sevenDaysRevenue}
+                myDataKey="Revenue"
+                lineDataKey="date"
+                detailedPage="revenue"
+                propertyId={propertyId}
+              />
+            </div>
+            <div className="box box3">
+              <TinyChartBox
+                money={false}
+                color="#3b82f6"
+                icon="/gas-station.svg"
+                title="Gallons Sold"
+                total={data?.totalGallonsSold}
+                chartData={data?.sevenDaysTotalGallons}
+                myDataKey="Gallons Sold"
+                lineDataKey="day"
+                detailedPage="gallons"
+                propertyId={propertyId}
+              />
+            </div>
+            <div className="box box4">
+              <PieChartBox
+                title="Top Selling Gasoline Products"
+                chartData={data?.topGasProducts}
+              />
+            </div>
+            <div className="box box5">
+              <TinyChartBox
+                money={true}
+                color="#0d9488"
+                icon="/cash.svg"
+                title="Cash Payments"
+                total={data?.dailyCashPayments}
+                chartData={data?.sevenDaysPaymentTotals}
+                myDataKey="Total Cash"
+                lineDataKey="day"
+                detailedPage="cash"
+                propertyId={propertyId}
+              />
+            </div>
+            <div className="box box6">
+              <TinyChartBox
+                money={true}
+                color="#7c3aed"
+                icon="/credit-card.svg"
+                title="Credit Card Payments"
+                total={data?.dailyCreditCardPayments}
+                chartData={data?.sevenDaysPaymentTotals}
+                myDataKey="Total Credit Card"
+                lineDataKey="day"
+                detailedPage="credit"
+                propertyId={propertyId}
+              />
+            </div>
+            <div className="box box7">
+              <BarChartBox
+                chartData={data?.sevenDaysRevenue}
+                color="#0d9488"
+                title="7 Day Revenue"
+                bar="Revenue"
+                xaxis="date"
+              />
+            </div>
+            <div className="box box8">
+              <BarChartBox
+                chartData={data?.sevenDaysTotalGallons}
+                color="#3b82f6"
+                title="7 Day Gallons Sold"
+                bar="Gallons Sold"
+                xaxis="day"
+              />
+            </div>
+
+            <div className="box box9">
+              <GasInventoryBoxes propertyId={propertyId} />
+            </div>
+
+            <div className="box box10">
+              <InventoryContainer propertyId={propertyId} />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 export default DashboardContainer;
--- client/src/Components/PropertyDropdown/PropertyDropdown.jsx
+++ client/src/Components/PropertyDropdown/PropertyDropdown.jsx
@@ -1,25 +1,53 @@
 import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-
+import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import { useAuth } from "../../utils/AuthContext";
 import newRequest from "../../utils/newRequest";
 import Dropdown from "../Dropdown/Dropdown";
+import Notification from "../Notification/Notification";
 import "./PropertyDropdown.scss";
+
 const PropertyDropdown = ({
   onPropertiesFetched = () => {},
   onPropertyChange,
   defaultSelected,
 }) => {
-  const [currentUser, setCurrentUser] = useState(
-    () => JSON.parse(localStorage.getItem("currentUser")) || ""
-  );
+  const [currentUser, setCurrentUser] = useState(null);
+  const [notification, setNotification] = useState(null);
+  const navigate = useNavigate();
+  const { logout } = useAuth();
 
-  const currentUserId = currentUser?.user.currentUserId;
+  useEffect(() => {
+    const storedUser = localStorage.getItem("currentUser");
+    if (storedUser) {
+      const parsedUser = JSON.parse(storedUser);
+      if (parsedUser && parsedUser.user && parsedUser.user.userId) {
+        setCurrentUser(parsedUser);
+      } else {
+        handleUnauthenticatedUser();
+      }
+    } else {
+      handleUnauthenticatedUser();
+    }
+  }, []);
+
+  const handleUnauthenticatedUser = () => {
+    setNotification({
+      type: "error",
+      message: "You need to login",
+    });
+    logout();
+    navigate("/login");
+  };
+
+  const currentUserId = currentUser?.user?.userId;
 
   const { data, isLoading, error } = useQuery(
     ["properties", currentUserId],
     async () => {
+      if (!currentUserId) return;
       const response = await newRequest.get(
         `/properties?userId=${currentUserId}`
       );
@@ -44,18 +72,28 @@ const PropertyDropdown = ({
   if (error) return "Error loading properties";
 
   return (
-    <Dropdown
-      options={data}
-      onChange={handleChange}
-      valueField="_id"
-      labelField="name"
-      value={defaultSelected}
-    />
+    <>
+      {notification && (
+        <Notification
+          type={notification.type}
+          message={notification.message}
+          duration={5000}
+          onClose={() => setNotification(null)}
+        />
+      )}
+      <Dropdown
+        options={data}
+        onChange={handleChange}
+        valueField="_id"
+        labelField="name"
+        value={defaultSelected}
+      />
+    </>
   );
 };
 
 PropertyDropdown.propTypes = {
-  onPropertyChange: PropTypes.func,
+  onPropertyChange: PropTypes.func.isRequired,
   defaultSelected: PropTypes.string,
   onPropertiesFetched: PropTypes.func,
 };
